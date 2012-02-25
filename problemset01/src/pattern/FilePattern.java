b3d93293dd44c448ca1e7e77e713ed142877a1c4
Joel Krebs <joel.krebs@students.unibe.ch>
2012-02-25 14:37:17 +0100

    added java classes for problemset01

---
+++ problemset01/src/pattern/FilePattern.java
@@ -0,0 +1,17 @@
+package pattern;
+
+import java.io.File;
+import java.io.FileFilter;
+
+public class FilePattern implements FileFilter {
+
+	public FilePattern(String string) {
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public boolean accept(File pathname) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
