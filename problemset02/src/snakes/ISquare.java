771583c4d823700e8f4ab786b69a766e802b9e0d
Michael Kohler <michaelkohler@live.com>
2012-03-05 22:02:29 +0100

    Problemset02 solved
    We solved the second problemset. We added a ScrambleUp and a InstantLose fields and tests for it. Also we documented the snakes.ISquare.
    Additional we added a patch files which contains all made changes.
    
    Squashed commit of the following:
    
    commit 54287843c00b8b48bf06d0bc8575b86e20c8db9a
    Author: Michael Kohler <michaelkohler@live.com>
    Date:   Mon Mar 5 22:01:30 2012 +0100
    
        Latest patch file for Problemset02 added.
    
        modified:   problemset02/problemset02.patch
    
    commit 2d6388a0345876678cddc22726937bae000b92d2
    Author: Michael Kohler <michaelkohler@live.com>
    Date:   Mon Mar 5 16:17:11 2012 +0100
    
        ScrableUpTest fixed
    
        modified:   problemset02/src/snakes/ScrambleUpTest.java
    
    commit 59d401bfb1f45afd4762122f1b752b40c7bf4732
    Author: Michael Kohler <michaelkohler@live.com>
    Date:   Mon Mar 5 14:12:05 2012 +0100
    
        Added documentation to snakes.ISquare
    
        This patch adds documentation to every method of snakes.ISquare
    
        modified:   problemset02/src/snakes/ISquare.java
    
    commit 1dd922c4d2be878ba2dcf729be3b1953366f443a
    Author: Michael Kohler <michaelkohler@live.com>
    Date:   Mon Mar 5 01:38:14 2012 +0100
    
        current problemset02.patch added to easily view made changes
    
    commit 171625a9b6ac89a7e2c998f9be3929b812dca269
    Author: Michael Kohler <michaelkohler@live.com>
    Date:   Mon Mar 5 01:32:00 2012 +0100
    
        Different changes
    
        ScrambleUp works as intended (manually tested)
        different changes and comments
        the ScrambleUp test is still failing but no idea why ATM
    
        modified:   problemset02/src/snakes/Game.java
        modified:   problemset02/src/snakes/InstantLose.java
        modified:   problemset02/src/snakes/Player.java
        modified:   problemset02/src/snakes/ScrambleUp.java
        modified:   problemset02/src/snakes/ScrambleUpTest.java
        modified:   problemset02/src/snakes/Square.java
    
    commit c131f1a9f833d1c4b3486765d31b6dd7d6da1615
    Author: Michael Kohler <michaelkohler@live.com>
    Date:   Sun Mar 4 15:00:40 2012 +0100
    
        All tasks done except 5 and the ScrambleUpTest still fails
    
        modified:   problemset02/src/snakes/Game.java
        modified:   problemset02/src/snakes/InstantLose.java
        modified:   problemset02/src/snakes/InstantLoseTest.java
        modified:   problemset02/src/snakes/Player.java
        modified:   problemset02/src/snakes/ScrambleUp.java
        modified:   problemset02/src/snakes/ScrambleUpTest.java
        modified:   problemset02/src/snakes/SimpleGameTest.java

--- problemset02/src/snakes/ISquare.java
+++ problemset02/src/snakes/ISquare.java
@@ -1,12 +1,63 @@
 package snakes;
 
 public interface ISquare {
+    /**
+     * Returns the position of the square
+     *
+     * @return      position of the square
+     */
 	public int position();
+	
+	/**
+     * This method returns the square to land on.
+     *
+     * @param  moves  the amount of moves the player makes
+     * @return        the square to land on
+     */
 	public ISquare moveAndLand(int moves);
+	
+	/**
+     * Returns whether the square is the first one or not.
+     *
+     * @return      boolean value whether it is the first square or not
+     */
 	public boolean isFirstSquare();
+	
+	/**
+     * Returns whether the square is the first one or not.
+     *
+     * @return      boolean value whether it is the last square or not
+     */
 	public boolean isLastSquare();
+	
+	/**
+     * Enters the square.
+     *
+     * @param  player  the player who should enter this field
+     * @return         void
+     */
 	public void enter(Player player);
+	
+	/**
+     * Leaves the square.
+     *
+     * @param  player  the player who should leave this field
+     * @return         void
+     */
 	public void leave(Player player);
+	
+	/**
+     * Returns whether this square is occupied or not.
+     *
+     * @return      occupation state of the square
+     */
 	public boolean isOccupied();
+	
+	/**
+     * Returns the square the requesting player should land on. Either it's this square
+     * or the first square if it's occupied.
+     *
+     * @return      returns the square to land on as object
+     */
 	public ISquare landHereOrGoHome();
 }
--- problemset02/src/snakes/ScrambleUpTest.java
+++ problemset02/src/snakes/ScrambleUpTest.java
@@ -19,7 +19,7 @@ public class ScrambleUpTest {
 		jack = new Player("Jack");
 		jill = new Player("Jill");
 		Player[] args = { jack, jill };
-		Game game = new Game(6, args);
+		Game game = new Game(12, args);
 		game.setSquareToScrambleUp(4);
 		assertTrue(game.notOver());
 		assertTrue(game.firstSquare().isOccupied());
@@ -31,14 +31,57 @@ public class ScrambleUpTest {
 	
 	@Given("newGame")
 	public Game jillToScrambleUp(Game game) {
-	    game.movePlayer(1);
+	    game.movePlayer(1); // jack lands on field 2
+	    assertTrue(game.notOver());
 	    assertEquals(2, jack.position());
 		assertEquals(1, jill.position());
-        game.movePlayer(3);
+		assertEquals(jill, game.currentPlayer());
+		
+        game.movePlayer(3); // jill lands on field 4 -> Scramble Up
 		assertTrue(game.notOver());
 		assertEquals(4, jack.position());
 		assertEquals(2, jill.position());
 		assertEquals(jack, game.currentPlayer());
+		
 		return game;
+	}
+	
+	@Given("jillToScrambleUp")
+    public Game notLandingOnField4DoesntScramble(Game game) {
+        game.movePlayer(1); // jack lands on field 5
+        assertTrue(game.notOver());
+        assertEquals(5, jack.position());
+        assertEquals(2, jill.position());
+        assertEquals(jill, game.currentPlayer());
+        
+        return game;
+    }
+	
+	@Given("notLandingOnField4DoesntScramble")
+	public Game jackToScrambleUp(Game game) {
+	    game.movePlayer(4); // jill lands on field 6
+	    assertTrue(game.notOver());
+	    assertEquals(6, jill.position());
+	    assertEquals(5, jack.position());
+	    assertEquals(jack, game.currentPlayer());
+	    
+	    game.movePlayer(1); // jack lands on field 6 -> back to 1
+	    assertTrue(game.notOver());
+	    assertEquals(1, jack.position());
+	    assertEquals(6, jill.position());
+	    assertEquals(jill, game.currentPlayer());
+	    
+	    game.movePlayer(1); // jill lands on field 7
+	    assertTrue(game.notOver());
+	    assertEquals(7, jill.position());
+	    assertEquals(1, jack.position());
+        assertEquals(jack, game.currentPlayer());
+	   
+	    game.movePlayer(3); // jack lands on field 4 -> ScrambleUp
+	    assertTrue(game.notOver());
+	    assertEquals(7, jack.position());
+	    assertEquals(4, jill.position());
+	    
+	    return game;
 	}
 }
