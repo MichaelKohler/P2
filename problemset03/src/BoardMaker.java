cfbe2ae36ffce9edc8a823aa93bab70667375bd7
Niko Schwarz <niko.schwarz@gmail.com>
2012-03-09 15:27:35 +0100

    Problemset03, ready to be solved.
    Aaron checked it!

---
+++ problemset03/src/BoardMaker.java
@@ -0,0 +1,11 @@
+import java.util.List;
+
+import org.codehaus.jparsec.Parser;
+
+
+class BoardMaker {
+	public boolean[][] makeBoardFrom(String description) {
+	    throw new RuntimeException();
+	}
+
+}
