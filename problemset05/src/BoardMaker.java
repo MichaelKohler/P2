bc7c1abb6db175dfef846a711dbf749fc35f6971
Niko Schwarz <niko.schwarz@gmail.com>
2012-03-24 16:29:25 +0100

    Problemset05:  Use the debugger to interpret the new turtle language.

---
+++ problemset05/src/BoardMaker.java
@@ -0,0 +1,12 @@
+import java.awt.geom.Line2D;
+import java.util.List;
+
+import org.codehaus.jparsec.Parser;
+
+class BoardMaker {
+	
+	public List<Line2D> makeBoardFrom(String instructionString) {
+		throw new RuntimeException();
+	}
+
+}
